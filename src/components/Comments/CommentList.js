340a45ffb1fc7279a3c35734c6d9ad324cc4b00e
vochithanh89 <61690947+vochithanh89@users.noreply.github.com>
2022-07-16 09:30:28 +0700

    disable more actions comment when logout

--- src/components/Comments/CommentList.js
+++ src/components/Comments/CommentList.js
@@ -112,6 +112,7 @@ function CommentList({ roomId }) {
                                                     handleSetCurrentCommentId(comment.id);
                                                     e.stopPropagation();
                                                 }}
+                                                disabled={!user}
                                             >
                                                 <BsThreeDots />
                                             </button>
