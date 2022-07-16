f7fcf43c1e494e37696d588c47c437cf133c95ad
vochithanh89 <61690947+vochithanh89@users.noreply.github.com>
2022-07-16 09:31:19 +0700

    add user info menu mobile

--- src/components/MenuModal/MenuModal.js
+++ src/components/MenuModal/MenuModal.js
@@ -3,20 +3,28 @@ import styles from './MenuModal.module.scss';
 import useModal from '@/hooks/useModal';
 import { NavLink } from 'react-router-dom';
 import { FaHome, FaCompass, FaHistory, FaSearch } from 'react-icons/fa';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiLogOut } from 'react-icons/fi';
 import clsx from 'clsx';
 import Image from '../shared/Image/Image';
 import guestImg from '@/assets/images/defaultAvatar.jpg';
 import { useDispatch } from 'react-redux';
 import { modalSlice } from '../redux/modalSlice';
+import useGetUserFirebase from '@/hooks/useGetUserFirebase';
+import { signOut } from 'firebase/auth';
+import { auth } from '@/firebase/config';
 
 function MenuModal({ children, className }) {
     const dispatch = useDispatch();
     const { isOpen, toggle } = useModal();
+    const user = useGetUserFirebase();
 
     const toggleLoginModal = () => {
         dispatch(modalSlice.actions.loginModalToggle());
         toggle();
+    };
+
+    const handleLogout = () => {
+        signOut(auth);
     };
 
     return (
@@ -32,15 +40,27 @@ function MenuModal({ children, className }) {
                 >
                     <h2 className={styles.title}>Menu</h2>
                     <div className={styles.menu}>
-                        <div className={styles.login}>
-                            <Image className={styles.avatar} src={guestImg} alt="guest avatar" />
-                            <h2 className={styles.userName}>Guest</h2>
-                            <p className={styles.text}>You need login</p>
-                            <button onClick={toggleLoginModal} className={styles.logBtn}>
-                                <FiLogIn className={styles.icon} />
-                                Login
-                            </button>
-                        </div>
+                        {user ? (
+                            <div className={styles.login}>
+                                <Image className={styles.avatar} src={user.photoURL} alt="guest avatar" />
+                                <h2 className={styles.userName}>{user.displayName}</h2>
+                                <p className={styles.text}>{user.email}</p>
+                                <button onClick={handleLogout} className={styles.logBtn}>
+                                    <FiLogOut className={styles.icon} />
+                                    Logout
+                                </button>
+                            </div>
+                        ) : (
+                            <div className={styles.login}>
+                                <Image className={styles.avatar} src={guestImg} alt="guest avatar" />
+                                <h2 className={styles.userName}>Guest</h2>
+                                <p className={styles.text}>You need login</p>
+                                <button onClick={toggleLoginModal} className={styles.logBtn}>
+                                    <FiLogIn className={styles.icon} />
+                                    Login
+                                </button>
+                            </div>
+                        )}
                         <NavLink to="/" className={(nav) => clsx(styles.menuItem, { [styles.active]: nav.isActive })}>
                             <FaHome className={styles.icon} />
                             <p className={styles.text}>Home</p>
