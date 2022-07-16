de6d3372e669bfaeb26d4a6dd2b87763cdc4d38e
vochithanh89 <61690947+vochithanh89@users.noreply.github.com>
2022-07-16 20:33:14 +0700

    remove react-snap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-import { hydrate, render } from 'react-dom';
+import { render } from 'react-dom';
 import App from './App';
 
 const rootElement = document.getElementById('root');
-if (rootElement.hasChildNodes()) {
-    hydrate(<App />, rootElement);
-} else {
-    render(<App />, rootElement);
-}
+render(<App />, rootElement);
